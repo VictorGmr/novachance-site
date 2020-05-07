91ccd4a17f05268b635b2ab64743e24d613e9344
Victor Guimaraes <victor.gmr@hotmail.com>
2020-05-07 00:22:11 -0300

    updated login interface, not functional

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,95 @@
 @extends('layouts.app')
 
+@section('title')
+Nova Chance - Login
+@endsection()
+
+@section('defineActive')
+	<li><a href="/home">INÍCIO</a></li>
+	<li><a href="/sobre">QUEM SOMOS</a></li>
+	<li><a href="/ncon">NC ON</a></li>
+	<li><a href="/discipulado">DISCIPULADO</a></li>
+	<li><a href="/contato">CONTATO</a></li>
+@endsection()
+
+@section('css')
+	<link rel="stylesheet" type="text/css" href="styles/contact.css">
+    <link rel="stylesheet" type="text/css" href="styles/contact_responsive.css">
+@endsection()
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<!-- Home -->
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <div class="home">
+		<div class="home_inner">
+			<!-- Image artist: https://unsplash.com/@yoannboyer -->
+			<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/contact.jpg" data-speed="0.8"></div>
+			<div class="home_container">
+				<div class="home_content text-center">
+					<div class="home_subtitle">Nova Chance</div>
+					<div class="home_title">Login</div>
+				</div>
+			</div>
+		</div>
+    </div>
+    
+    <!-- Contact -->
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+	<div class="contact">
+		<div class="container">
+			<div class="row">
+				
+				<!-- Contact Form -->
+				<div class="col-lg-12">
+					<div class="contact_form_container" style="max-width=200%;text-align:center;position:relative;">
+						<div class="contact_title">Entre com seus dados:</div>
+						<form action="{{ route('login') }}" method="POST" class="contact_form" id="contact_form">
+                            @csrf
 
+                            <div>
+                                <input type="email" style="width:100%;" class="contact_input @error('email') is-invalid @enderror" placeholder="E-mail" autocomplete="email" required="required" autofocus>
+                                
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                                
+                                <input type="password" style="width:100%;" class="contact_input @error('password') is-invalid @enderror" placeholder="Senha" required="required">
+                            
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+                               
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                                
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
+							
+                            <div style="text-align:center;">
+                                <button style="display:inline-block;" type="submit"class="contact_button">Entrar</button>
+                                
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a style="display:inline-block;" class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
                             </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+                
 @endsection
+
+@section('js')
+<script src="js/contact.js"></script>
+@endsection()
